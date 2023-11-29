1ccb7b3c78320fa91e1995021102127ec8446f2f
weyfelix <55450176+weyfelix@users.noreply.github.com>
2023-11-29 01:42:38 -0300

    creating the header layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
+    <div className='container'>
+        <div className='header'>
+              <img src="./src/assets/Logo.png" alt="Logo ToDo List" />
+        </div>
+        <div className='corpo'>
+          <div className='inputTask'>
+              <input type="text" placeholder='Adicione uma nova tarefa'/>
+              <button className='buttonCriar'>Criar <img src="./src/assets/plus.png" alt="" /></button>
+          </div>
+          <div className='containerTaks'>
+
+          </div>
+        </div>
+    </div>
   )
 }
 
